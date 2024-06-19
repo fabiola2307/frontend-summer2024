24c3585c4d10796dacc0622d6d81b5a184b98fc9
fabiola2307 <146094973+fabiola2307@users.noreply.github.com>
2024-06-18 18:26:17 -0700

    pertemuan-5

--- index.js
+++ index.js
@@ -1,149 +1,49 @@
-//Array & Objek
-//Deklarasi array
-//Cara 1.Array literal
-let number = [1, 2, 3, 4, 5]
-//Cara 2. Kata kunci new
-let number2 = new Array(6, 7, 8, 9, 10)
+//JavaScript Loop
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
 
-//Tipe data dalam array
-let number = [1, 2, 3, 4, 5] //number
-let student = ["John", "Jane", "Bob"]; //string
-let john = ["John", 30, true]; //campuran
+//while loop
+let i = 1;
 
-console.log(numbers);
-console.log(students);
-console.log(john);
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
 
-//Mengakses elemennt didalam array
-//Memulai index
-console.log(numbers[3]); //4
-console.log(students[1]) //Jane
-console.log(john[3]); //[80,90,100]
-console.log(john[3][1]) //90
+//do while Loop
+let i = 1;
 
-numbers[30] = 100;
-console.log(numbers);
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
 
-//array length
-console.log(numbers.length); //5
-console.log(numbers.length); //3
+//Array built-in method
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]); 
 
-//Mengaksess elemen terakhir
-console.log(numbers(numbers,length -1));
+//Array.forEach()
+ array.forEach(function (value, index, array){
+    // kode program
+    })
 
-//Array Method
-let array = [1, 2, 3, "halo", false, true];
-console.log(array);
-console.log(array.toString()); // Output: 1,2,3,halo,false,true
-console.log(array.join("_")); 
+    let colors = ['blue', 'red', 'yellow', 'green'];
 
-array.pop();
-console.log(array);
-array.push("Selamat Pagi");
-console.log(array);
-array.shift();
-console.log(array);
-array.unshift("Selamat Pagi")
-console.log(array);
+    colors.forEach(function (data) {
+      console.log(data)
+    });
+    
+    
+    let colors = ['blue', 'red', 'yellow', 'green'];
 
-//Menggabungkan Lebih Dari Satu Array dengan concat()
-let buah = ["pisang", "apel", "jeruk"];
-let sayur = ["tomat", "bayam", "wortel"];
-let biji = ["kedelai", "kacang tanah", "kacang polong"];
-
-let makanan = buah.concat(sayur, biji);
-console.log(makanan); 
-
-//Mengambil Element Array dengan slice()
-let cemilan = ["kripik singkong", "kripik kentang", "krupuk ikan", "permen", "coklat", "kue"];
-let cemilanGurih = cemilan.slice(0, 3);
-let cemilanManis = cemilan.slice(3);
-console.log(cemilanGurih); 
-
-let cemilan = ["kripik singkong", "kripik kentang", "krupuk ikan", "permen", "coklat", "kue"];
-let cemilanGurih = cemilan.slice(0, 3);
-let cemilanManis = cemilan.slice(3);
-
-console.log(cemilanGurih); // Output: ["kripik singkong", "kripik kentang", "krupuk ikan"]
-console.log(cemilanManis); // Output: ["permen", "coklat", "kue"]
-
-//Menggunakan Object Literal
-let buah = { nama: 'mangga', warnaKulit: 'hijau', hargaPerBuah: 5000 };
-
-let orang = {
-  nama: 'john',
-  umur: 24,
-  pekerjaan: 'programmer'
-};
-
-//Menggunakan Kata Kunci new
-let orang = new Object();
-
-orang.nama = 'john';
-orang.umur = 24;
-orang.pekerjaan = 'programmer';
-
-//Mengakses Properti Objek
-let orang = {
-    nama: 'john',
-    umur: 24,
-    pekerjaan: 'programmer'
-  };
-  
-  console.log(orang.nama); // Output: "john"
-  console.log(orang.umur); // Output: 24
-  console.log(orang.pekerjaan); // Output: "programmer"
-  
-  //Bracket Notation
-  let orang = {
-    nama: 'john',
-    umur: 24,
-    pekerjaan: 'programmer'
-  };
-  
-  console.log(orang["nama"]); // Output: "john"
-  console.log(orang['umur']); // Output: 24
-  console.log(orang["pekerjaan"]); // Output: "programmer"
-  //(Perlu kita perhatikan yang berikut ini ketika kita mau mengakses properti objek
-  // secara dynamic (dinamis). Yang dimaksud dengan mengakses properti objek secara dinamis
-  // adalah ketika kita menggunakan nama dari suatu variabel sebagai nama properti saat 
-  //mengakses properti objek tersebut.)
-
-  const smartphone = {
-    kamera: "10MP",
-    memori: "128GB"
-};
-
-const fitur = "kamera";
-
-//Bracket Notation
-let objek = {
-    namaProperti: nilaiProperti
- };
- 
- // Bracket Notation
- objek["namaProperti"] // Output: nilaiProperti
- 
- // bisa juga menggunakan single quote
- objek['namaProperti'] // Output: nilaiProperti
-
-//Menambah Properti Baru pada Objek
-let orang = {
-    nama: 'john',
-    umur: 24,
-    pekerjaan: 'programmer'
-  };
-  
-  orang.kebangsaan = 'warga negara indonesia';
-  
-  console.log(orang);
-  /*
-    Output:
-    {
-      nama: 'john',
-      umur: 24,
-      pekerjaan: 'programmer',
-      kebangsaan: 'warga negara indonesia'
-    }
-  */
-  
+    colors.forEach(function (data, idx) {
+      if(idx % 2 === 0) { // melakukan pengecekan jika index elemen adalah bilangan genap
+        console.log(data, idx)
+      } else {
+        console.log('not allowed', idx)
+      }
+    });
+    
+    
